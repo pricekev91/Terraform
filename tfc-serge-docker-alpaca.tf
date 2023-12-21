c5bdfa59c8d57ed453e836a51c4c7d35b250e559
pricekev91 <55143215+pricekev91@users.noreply.github.com>
2023-12-21 12:20:57 -0500

    Update tfc-serge-docker-alpaca.tf

--- tfc-serge-docker-alpaca.tf
+++ tfc-serge-docker-alpaca.tf
@@ -1,6 +1,13 @@
 provider "null" {}
 
+resource "null_resource" "install_git" {
+  provisioner "local-exec" {
+    command = "sudo apt install git -y"
+  }
+}
+
 resource "null_resource" "install_docker" {
+  depends_on = [null_resource.install_git]
   provisioner "local-exec" {
     command = <<-EOF
       sudo apt update
@@ -13,25 +20,22 @@ resource "null_resource" "install_docker" {
   }
 }
 
-resource "null_resource" "install_git" {
-  provisioner "local-exec" {
-    command = "sudo apt install git -y"
-  }
-}
-
 resource "null_resource" "clone_serge" {
+  depends_on = [null_resource.install_docker]
   provisioner "local-exec" {
     command = "sudo git clone https://github.com/serge-chat/serge"
   }
 }
 
 resource "null_resource" "build_docker_image" {
+  depends_on = [null_resource.clone_serge]
   provisioner "local-exec" {
     command = "cd serge && sudo docker build -t serge ."
   }
 }
 
 resource "null_resource" "run_docker_container" {
+  depends_on = [null_resource.build_docker_image]
   provisioner "local-exec" {
     command = "sudo docker run -d --restart always -p 8008:8008 -v /mnt/ai-models/WizardLM-Uncensored-30B.bin:/usr/src/app/weights/WizardLM-Uncensored-30B.bin serge"
   }
